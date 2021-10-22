a8b56a738635ebca329152862f34425aa95cc65e
Kevin Hartwig <kontakt@kevinhartwig.de>
2021-10-22 22:05:33 +0200

    server update

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://api.kevinhartwig.de",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1333fb804bb47d1770d16f39ac4ae224'),
+      secret: env("ADMIN_JWT_SECRET", "1333fb804bb47d1770d16f39ac4ae224"),
     },
   },
 });
